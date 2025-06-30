fed043be002a9f43c0cb23f89fa6c378df8203ee
teja9491 <gunupurutejaswani@gmail.com>
2025-06-30 19:28:10 +0530

    ff32

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,29 +1,17 @@
 import { clerkClient } from "@clerk/express";
-
 export const protectAdmin = async (req, res, next) => {
   try {
     const { userId } = req.auth();
-
-    if (!userId) {
-      return res.status(401).json({
-        success: false,
-        message: "Unauthorized: No user ID found in request",
-      });
-    }
-
     const user = await clerkClient.users.getUser(userId);
-
-    if (user.privateMetadata.role !== "admin") {
-      return res.status(403).json({
+    if (user.privateMetadata.role != "admin") {
+      return res.json({
         success: false,
-        message: "You are not authorized",
+        message: "You are not authorized ",
       });
     }
-
     next();
-  } catch (err) {
-    console.error("❌ protectAdmin error:", err);
-    return res.status(500).json({
+  } catch {
+    return res.json({
       success: false,
       message: "Something went wrong while checking admin role",
     });
--- server/models/User.js
+++ server/models/User.js
@@ -19,6 +19,5 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-
 const User = mongoose.model("User", userSchema);
 export default User;
